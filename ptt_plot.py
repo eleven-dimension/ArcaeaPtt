8f210afcd1a3395a7dd1cb392e6ea547e3807e2c
eleven-dimension <3096152319@qq.com>
2021-09-11 13:21:09 +0800

    ADD ptt_plot.py AND ptt.txt

---
+++ ptt_plot.py
@@ -0,0 +1,20 @@
+from datetime import datetime 
+import matplotlib.pyplot as plt 
+
+with open("ptt.txt",'r',encoding='utf-8') as f:
+    ptt_lines = f.readlines()
+
+x_time, y_ptt = [], []
+for line in ptt_lines:
+    data = line.split()
+    x_time.append(data[0])
+    y_ptt.append(float(data[1]))
+
+x_date_format = [datetime.strptime(d, '%Y/%m/%d').date() for d in x_time]
+
+plt.title('Ptt')
+plt.plot(x_date_format, y_ptt, 'o-')
+plt.xlabel('Time')
+plt.ylabel('Ptt')
+
+plt.show()
